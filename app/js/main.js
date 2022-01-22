81b41ec54d6c342d8678a93a73d8d14cb4e9d95a
mr-dim-ka <kalinin9.7@mail.ru>
2022-01-22 20:01:01 +0600

    added section gallery

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,3 @@
-// $(function () {
-	
-// });
+$(function () {
+	var mixer = mixitup('.gallery__items');
+});
--- gulpfile.js
+++ gulpfile.js
@@ -97,7 +97,8 @@ function css() {
 function style() {
 	return src([
 		'node_modules/normalize.css/normalize.css',
-		'node_modules/slick-carousel/slick/slick.css'
+		'node_modules/slick-carousel/slick/slick.css',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
 	])
 		// .pipe(concat('libs.min.css'))
 		.pipe(concat('_libs.scss'))
@@ -107,8 +108,10 @@ function style() {
 
 function script() {
 	return src([
-		'node_modules/slick-carousel/slick/slick.js'
-		// 'node_modules/jquery/dist/jquery.js'
+		'node_modules/slick-carousel/slick/slick.js',
+		// 'node_modules/jquery/dist/jquery.js',
+		'node_modules/mixitup/dist/mixitup.js',
+		'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
 	])
 		.pipe(concat('libs.min.js'))
 		.pipe(uglify())
